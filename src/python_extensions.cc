1e398ef3432f09798ea120ebb7b83836b06b99bc
Harry Xie <harry1996@gmail.com>
2020-04-08 22:01:30 +0100

    added python_extensions.cc

---
+++ src/python_extensions.cc
@@ -0,0 +1,14 @@
+#include <pybind11/stl.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
+
+void init_ball(pybind11::module & m);
+void init_field(pybind11::module & m);
+void init_geometry(pybind11::module & m);
+
+PYBIND11_MODULE(rlutilities, m) {
+  pybind11::module simulation = m.def_submodule("simulation");
+	init_ball(simulation);
+	init_field(simulation);
+	init_geometry(simulation);
+}
