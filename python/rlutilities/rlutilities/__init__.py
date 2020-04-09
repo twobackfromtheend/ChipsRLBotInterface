489e1953de3b4688d5927fa8e9964d1d173876d5
Harry Xie <harry1996@gmail.com>
2020-04-09 11:21:12 +0100

    added linear_algebra to __init__.py

--- python/rlutilities/rlutilities/__init__.py
+++ python/rlutilities/rlutilities/__init__.py
@@ -1,4 +1,5 @@
 import sys
-from .rlutilities import simulation
+from .rlutilities import simulation, linear_algebra
 
 sys.modules["rlutilities.simulation"] = simulation
+sys.modules["rlutilities.linear_algebra"] = linear_algebra
